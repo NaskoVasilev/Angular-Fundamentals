56391f354ca3d48c4b8db6252c9b502faebb871b
Atanas Vasilev <nasko01vasilev@gmail.com>
2019-11-17 16:29:19 +0200

    Remove some unnecessary files and lines of code

--- slido/src/app/auth/login/login.component.ts
+++ slido/src/app/auth/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/core/services/auth.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -14,7 +13,6 @@ export class LoginComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private authService: AuthService,
-    private router: Router
   ) { }
 
   ngOnInit() {
